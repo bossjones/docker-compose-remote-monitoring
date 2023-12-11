bc6c3cc00925835f5efb7ac215246e3de7772937
Malcolm Jones <bossjones@theblacktonystark.com>
2023-12-11 16:05:36 -0500

    wip: systemd fix

--- systemd/scripts/promtail-bin-install.sh
+++ systemd/scripts/promtail-bin-install.sh
@@ -23,6 +23,23 @@ chown promtail:promtail /tmp/positions.yaml
 
 systemctl cat promtail
 
-cp -a promtail/promtail-gateway.yaml /etc/promtail/config.yml
+cp -a ../../promtail/promtail-gateway.yaml /etc/promtail/config.yml
+
+# ---------------------------------
+
+# create a heredoc that writes to overrides.conf
+mkdir -p /etc/systemd/system/promtail.service.d/ || true
+cat <<EOF >/etc/systemd/system/promtail.service.d/overrides.conf
+[Service]
+Environment=NODENAME=$(hostname)
+EOF
+
+echo -e "\n"
+
+cat /etc/systemd/system/promtail.service.d/overrides.conf
+
+systemctl daemon-reload
+
+# ---------------------------------
 
 systemctl restart promtail
